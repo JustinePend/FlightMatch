bc5c5ce7241349bd443487d1d45d7ee22234893b
Jessica Chew <jessicacheww@ucla.edu>
2023-03-09 22:19:03 -0800

    Implement create account
    - Add home.hbs, login.hbs, protected.hbs, register.hbs
    - Add checker for registered accounts
    - Add password hashing

--- flightmatch/server/server.js
+++ flightmatch/server/server.js
@@ -1,14 +1,99 @@
 const express = require("express");
+const exphbs = require('express-handlebars');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/arrivals"));
 app.use(require("./routes/departures"));
 // get driver connection
 const dbo = require("./db/conn");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// To parse cookies from the HTTP Request
+app.use(cookieParser());
+
+app.engine('hbs', exphbs.engine({
+    extname: '.hbs'
+}));
+
+app.set('view engine', 'hbs');
+
+// Our requests hadlers will be implemented here...
+app.listen(3000);
+app.get('/', function (req, res) {
+  res.render('home');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+const crypto = require('crypto');
+
+const getHashedPassword = (password) => {
+    const sha256 = crypto.createHash('sha256');
+    const hash = sha256.update(password).digest('base64');
+    return hash;
+}
+
+const users = [
+  // This user is added to the array to avoid creating a new user on each restart
+  {
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'johndoe@email.com',
+      // This is the SHA256 hash for value of `password`
+      password: 'XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg='
+  }
+];
+
+app.post('/register', (req, res) => {
+  const { email, firstName, lastName, password, confirmPassword } = req.body;
+
+  // Check if the password and confirm password fields match
+  if (password === confirmPassword) {
+
+      // Check if user with the same email is also registered
+      if (users.find(user => user.email === email)) {
+
+          res.render('register', {
+              message: 'User already registered.',
+              messageClass: 'alert-danger'
+          });
+
+          return;
+      }
+
+      const hashedPassword = getHashedPassword(password);
+
+      // Store user into the database if you are using one
+      users.push({
+          firstName,
+          lastName,
+          email,
+          password: hashedPassword
+      });
+
+      res.render('login', {
+          message: 'Registration Complete. Please login to continue.',
+          messageClass: 'alert-success'
+      });
+  } else {
+      res.render('register', {
+          message: 'Password does not match.',
+          messageClass: 'alert-danger'
+      });
+  }
+});
 
 app.listen(port, () => {
   // perform a database connection when server starts
@@ -18,3 +103,5 @@ app.listen(port, () => {
   });
   console.log(`Server is running on port: ${port}`);
 });
+
+
