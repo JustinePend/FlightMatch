79be1493491f1cfb4705fac7a84077490573226d
lessthan-3 <niklasellegaard123@gmail.com>
2023-03-11 10:34:27 -0800

    add profile database support

---
+++ flightmatch/server/routes/profiles.js
@@ -0,0 +1,84 @@
+const express = require("express");
+
+// profiles is an instance of the express router.
+// We use it to define our routes.
+// The router will be added as a middleware and will take control of requests starting with path /profiles.
+const profiles = express.Router();
+
+// This will help us connect to the database
+const dbo = require("../db/conn");
+
+// This help convert the id from string to ObjectId for the _id.
+const ObjectId = require("mongodb").ObjectId;
+
+
+// This section will help you get a list of all the profiles flights.
+profiles.route("/profiles").get(function (req, res) {
+  let db_connect = dbo.getDb("flights");
+  db_connect
+    .collection("profiles")
+    .find({})
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+// This section will help you get a single profiles flight by id
+profiles.route("/profiles/:id").get(function (req, res) {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId( req.params.id )};
+  db_connect
+      .collection("profiles")
+      .findOne(myquery, function (err, result) {
+        if (err) throw err;
+        res.json(result);
+      });
+});
+
+// This section will help you create a new profiles flight.
+profiles.route("/profiles/add").post(function (req, response) {
+  let db_connect = dbo.getDb();
+  let myobj = {
+    name: req.body.name,
+    uid: req.body.uid,
+    email: req.body.email,
+  };
+  db_connect.collection("profiles").insertOne(myobj, function (err, res) {
+    if (err) throw err;
+    response.json(res);
+  });
+});
+
+// This section will help you update an profile by id.
+profiles.route("/update/:id").post(function (req, response) {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId( req.params.id )};
+  let newvalues = {
+    $set: {
+        name: req.body.name,
+        uid: req.body.uid,
+        email: req.body.email,
+    },
+  };
+  db_connect
+    .collection("profiles")
+    .updateOne(myquery, newvalues, function (err, res) {
+      if (err) throw err;
+      console.log("1 document updated");
+      response.json(res);
+    });
+});
+
+// This section will help you delete an profiles flight
+profiles.route("/:id").delete((req, response) => {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId( req.params.id )};
+  db_connect.collection("profiles").deleteOne(myquery, function (err, obj) {
+    if (err) throw err;
+    console.log("1 document deleted");
+    response.json(obj);
+  });
+});
+
+module.exports = profiles;
