0cd541c4f2d0706796c8e245ac4c25c65c26361b
Rebekah <rebapatel@gmail.com>
2023-03-16 11:54:30 -0700

    Updating About page.

--- flightmatch/client/src/components/about.js
+++ flightmatch/client/src/components/about.js
@@ -6,12 +6,13 @@ export default function About() {
     <div>
       <h1>About Us!</h1>
         <body>
-          Hello! We are students at UCLA and this is our final project for our CS35L class.
+          Hello! We are students at UCLA and this is our final project for our Computer Science
+          35L Software Construction class.
           We have all experienced the absolute chaotic mess it can be when its holiday break and
-          thousands of students are trying to coordinate car rides to LAX.
+          thousands of students are trying to coordinate car rides from LAX.
 
-          We wanted to create a solution that would provides students a cheaper and more efficient 
-          option to finding airport rides. 
+          We wanted to create a solution that would provide students a cheaper and more efficient 
+          option to finding airport rides. We hope this helps you! Thank you!
         </body>
     </div>
   );
